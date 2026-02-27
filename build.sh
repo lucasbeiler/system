7993209796ec12fb0477a8d8d83ccde4b1217478
Lucas Beiler <lucasbeiler@protonmail.com>
2026-02-27 00:01:53 -0300

    Init repository

---
+++ build.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+set -euo pipefail
+
+# Architecture and Alpine version.
+ARCH=$(uname -m)
+VERSION=v3.23
+MIRROR=https://dl-cdn.alpinelinux.org/alpine
+WORKDIR=/hardenedos/rootfs
+OUTPUT=/hardenedos/rootfs.erofs
+ENABLE_TESTING=false
+
+# Require root. TODO: All the building process should run in a REPRODUCIBLE and ROOTLESS environment as soon as possible! 
+if [ "$EUID" -ne 0 ]; then echo "Be root!"; exit 1; fi;
+
+# Clean up previous work
+umount ${WORKDIR}/dev ${WORKDIR}/proc ${WORKDIR}/sys 2>/dev/null || :
+rm -rf $WORKDIR && mkdir -p $WORKDIR
+
+# Prepare chroot with pseudofilesystems.
+mkdir -p ${WORKDIR}/proc ${WORKDIR}/dev ${WORKDIR}/sys
+for pseudofs in proc sys dev; do mount -o bind /${pseudofs} ${WORKDIR}/${pseudofs}; done
+
+# Prepare basic DNS connectivity inside the chroot.
+mkdir -p ${WORKDIR}/etc/
+echo "nameserver 9.9.9.9" > ${WORKDIR}/etc/resolv.conf
+
+# Set up the official package repositories. 
+# In the future, I'll have my own repositories for some specific packages (hardened builds of Chromium, iwd, and other security-critical software). 
+# And, also, I'll continuously test for Reproducible Builds for the packages that I use from the Alpine repositories.
+mkdir -p ${WORKDIR}/etc/apk/keys
+echo "$MIRROR/$VERSION/main" >> $WORKDIR/etc/apk/repositories
+echo "$MIRROR/$VERSION/community" >> $WORKDIR/etc/apk/repositories
+if [[ $VERSION == "edge" || $ENABLE_TESTING == true ]]; then
+  echo "$MIRROR/edge/testing" >> $WORKDIR/etc/apk/repositories
+fi
+
+# Install the base packages.
+apk --keys-dir=/usr/share/apk/keys/ -p $WORKDIR --arch $ARCH --initdb --no-cache add alpine-base alpine-baselayout acpid busybox busybox-suid musl-utils openssl argon2 fortify-headers iwd e2fsprogs e2fsprogs-extra util-linux tpm2-tools tpm2-tss-dev tpm2-tss-tcti-device fscrypt fscryptctl \
+  iptables pipewire-pulse pipewire wireplumber pavucontrol apparmor apparmor-profiles apparmor-utils checksec-rs dnscrypt-proxy curl \
+  cryptsetup dbus font-terminus mesa linux-firmware-intel sof-firmware linux-stable linux-stable-dev linux-headers pciutils chromium bash eudev udev-init-scripts \
+  sway river mako grim fastfetch slurp flameshot swaybg swaylock swayidle kanshi fuzzel yazi waybar alacritty font-dejavu libinput jq git sbctl cryptsetup-dev argon2 argon2-dev argon2-libs musl-dev \
+  mesa-dri-gallium mesa-va-gallium iproute2-minimal openssh-client-default mesa-vulkan-intel intel-media-driver libva-intel-driver linux-firmware-i915 linux-firmware-xe intel-ucode wayland-protocols \
+  alsa-utils alsaconf pipewire-alsa pipewire-jack
+
+apk -p $WORKDIR --arch $ARCH --no-cache -X https://dl-cdn.alpinelinux.org/alpine/edge/testing add ntpd-rs hardened-malloc ukify systemd-efistub
+
+# TODO: Copy Secure Boot public keys / certs to /var/lib/sbctl/keys/{PK,KEK,db}/*.pem
+# TODO: ... then add sbctl's binary and /var/lib/sbctl/keys/ to the initramfs.
+# TODO: ... then run sbctl enroll-keys if `sbctl status` shows, in the initramfs, that Secure Boot is enabled in its setup mode.
+# Populate root with some configuration files and such.
+cp -r root_files/* ${WORKDIR}/
+
+# TODO: Do not build as root like this.
+# chroot $WORKDIR sh -c "cd /usr/src/crypt/tpm2_luks && cargo build --release && install -m 755 target/release/tpm2_luks /usr/local/bin/crypter"
+# [ -f $WORKDIR/usr/local/bin/crosvm ] || chroot $WORKDIR sh -c 'apk del rust cargo && apk add llvm-dev libcap-dev git rustup wayland-dev gcc clang python3 libcap dtc py3-rich py3-argh && git clone https://chromium.googlesource.com/crosvm/crosvm && cd crosvm && git submodule update --init && rustup-init -y && . "$HOME/.cargo/env" && RUSTFLAGS="-C target-feature=-crt-static" cargo build --release --features gpu && install -m 755 target/release/crosvm /usr/local/bin/crosvm && rm -rf /crosvm /root/.rustup /root/.cargo'
+
+chroot $WORKDIR rc-update add acpid default
+chroot $WORKDIR rc-update add bootmisc boot
+chroot $WORKDIR rc-update add crond default
+chroot $WORKDIR rc-update add dbus
+chroot $WORKDIR rc-update add devfs sysinit
+chroot $WORKDIR rc-update add dmesg sysinit
+chroot $WORKDIR rc-update add hostname boot
+chroot $WORKDIR rc-update add hwclock boot
+chroot $WORKDIR rc-update add hwdrivers sysinit
+chroot $WORKDIR rc-update add iptables
+chroot $WORKDIR rc-update add iwd boot
+chroot $WORKDIR rc-update add killprocs shutdown
+chroot $WORKDIR rc-update add mdev sysinit
+chroot $WORKDIR rc-update add modules boot
+chroot $WORKDIR rc-update add mount-ro shutdown
+chroot $WORKDIR rc-update add networking boot
+chroot $WORKDIR rc-update add ntpd-rs
+chroot $WORKDIR rc-update add savecache shutdown
+chroot $WORKDIR rc-update add seedrng boot
+chroot $WORKDIR rc-update add setup-crosvm-net default
+chroot $WORKDIR rc-update add swap boot
+chroot $WORKDIR rc-update add sysctl boot
+chroot $WORKDIR rc-update add udev sysinit
+chroot $WORKDIR rc-update add udev-postmount default
+chroot $WORKDIR rc-update add udev-settle sysinit
+chroot $WORKDIR rc-update add udev-trigger sysinit
+chroot $WORKDIR rc-update del networking boot
+# chroot $WORKDIR rc-update add apparmor boot # TODO: Needs a patched kernel to work.
+# chroot $WORKDIR rc-update add dnscrypt-proxy # TODO: Patch its configuration to my liking.
+
+chroot $WORKDIR setup-desktop sway
+
+# Lock root user forever. # TODO: Keep uncommented.
+# chroot $WORKDIR passwd -l root 
+
+cp -r ${WORKDIR}/etc/shadow ${WORKDIR}/etc/shadow.bkp
+cp -r ${WORKDIR}/etc/passwd ${WORKDIR}/etc/passwd.bkp
+cp -r ${WORKDIR}/etc/group  ${WORKDIR}/etc/group.bkp
+chroot ${WORKDIR} ln -sf /data/etc/shadow /etc/shadow
+chroot ${WORKDIR} ln -sf /data/etc/passwd /etc/passwd
+chroot ${WORKDIR} ln -sf /data/etc/group /etc/group
+rm -rf ${WORKDIR}/home/ ${WORKDIR}/var/lib/iwd/ ${WORKDIR}/var/empty/ ${WORKDIR}/var/lock/ ${WORKDIR}/var/log/
+chroot ${WORKDIR} ln -sf /data/home /home
+chroot ${WORKDIR} ln -sf /data/var/lib/iwd /var/lib/iwd
+chroot ${WORKDIR} ln -sf /data/var/empty /var/empty
+chroot ${WORKDIR} ln -sf /data/var/log /var/log
+chroot ${WORKDIR} ln -sf /data/var/lock /var/lock
+mkdir ${WORKDIR}/data
+
+# Enable my services.
+chroot $WORKDIR rc-update add setup-crosvm-net default
+
+# Change VERSION_ID.
+sed -i 's/^VERSION_ID=.*/VERSION_ID='"$OS_BUILD_TAG"'/' ${WORKDIR}/etc/os-release
+
+# Umount things.
+umount ${WORKDIR}/dev ${WORKDIR}/proc ${WORKDIR}/sys 2>/dev/null
+
+# Create an image.
+rm -f $OUTPUT
+mkfs.erofs -L ${OS_BUILD_TAG} -zlz4hc $OUTPUT $WORKDIR
+VERITY_INFO=$(veritysetup format "$OUTPUT" "${OUTPUT}.verity")
+echo "Done: $OUTPUT"
+
+# The rootfs image is already done and verity'ed, so let's (re)create the initramfs now...
+echo "$VERITY_INFO" | awk '/Root hash:/ {print $3}' | tee ${WORKDIR}/verityhash
+echo "${OS_BUILD_TAG}" > ${WORKDIR}/os_build_tag
+chroot ${WORKDIR} mkinitfs $(ls ${WORKDIR}/lib/modules | head -n1)
+
+# Generate Unified Kernel Image
+chroot ${WORKDIR} ukify build \
+    --output "/boot/uki.efi" \
+    --cmdline "root=/dev/mapper/root" \
+    --microcode "/boot/intel-ucode.img" \
+    --linux "/boot/vmlinuz-stable" \
+    --initrd "/boot/initramfs-stable"
+
+rm ${WORKDIR}/boot/vmlinuz* ${WORKDIR}/boot/initramfs*
+
+mv /usr/lib/systemd/boot/efi/systemd-bootx64.efi /hardenedos/bootloader.efi
+sbsign --key /tmp/sbsign/keys/db/db.key \
+       --cert /tmp/sbsign/keys/db/db.pem \
+       --output /hardenedos/bootloader-signed.efi /hardenedos/bootloader.efi
+
+sbsign --key /tmp/sbsign/keys/db/db.key \
+       --cert /tmp/sbsign/keys/db/db.pem \
+       --output ${WORKDIR}/boot/uki-${OS_BUILD_TAG}-signed.efi ${WORKDIR}/boot/uki.efi
---
+++ update.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+# update.sh
+# Performs an A/B update from the latest GitHub Release.
+# Usage: sudo bash update.sh
+set -euo pipefail
+
+RELEASE_TAG="$(curl -s https://api.github.com/repos/lucasbeiler/system/releases | grep -i "os-" | grep '"name"' | head -1 | cut -d'"' -f4 | sed 's/[^0-9]//g')"
+RELEASE_URL="https://github.com/lucasbeiler/system/releases/download/${RELEASE_TAG}"
+WORKDIR="$(mktemp -d)"
+
+# Helpers
+info() { echo -e "\e[1;32m[INFO]\e[0m  $*"; }
+die()  { echo -e "\e[1;31m[ERR]\e[0m   $*" >&2; exit 1; }
+
+[[ $EUID -eq 0 ]] || die "Run as root."
+
+# 1. Download artifacts
+trap 'rm -rf "$WORKDIR"' EXIT
+info "Downloading artifacts (build ${RELEASE_TAG})..."
+curl -L -f --progress-bar -o "$WORKDIR/bootloader-signed.efi"                "$RELEASE_URL/bootloader-signed.efi"
+curl -L -f --progress-bar -o "$WORKDIR/uki-${RELEASE_TAG}-signed.efi"        "$RELEASE_URL/uki-${RELEASE_TAG}-signed.efi"
+curl -L -f --progress-bar -o "$WORKDIR/rootfs.erofs"                      "$RELEASE_URL/rootfs.erofs"
+curl -L -f --progress-bar -o "$WORKDIR/rootfs.erofs.verity"               "$RELEASE_URL/rootfs.erofs.verity"
+
+# TODO: Verify signatures.
+
+# 2. Detect current slot
+CURRENT_ROOT="$(findmnt -n -o SOURCE /)"
+info "Current root device: $CURRENT_ROOT"
+
+DISK="/dev/$(lsblk -no PKNAME "$CURRENT_ROOT" | head -1)"
+[[ -b "$DISK" ]] || die "Could not determine parent disk of $CURRENT_ROOT."
+
+if [[ "$DISK" =~ nvme|mmcblk ]]; then
+    P="${DISK}p"
+else
+    P="${DISK}"
+fi
+
+if [[ "$CURRENT_ROOT" == "${P}2" ]]; then
+    CURRENT_SLOT="a"
+    TARGET_ROOT="${P}4"
+    TARGET_VERITY="${P}5"
+elif [[ "$CURRENT_ROOT" == "${P}4" ]]; then
+    CURRENT_SLOT="b"
+    TARGET_ROOT="${P}2"
+    TARGET_VERITY="${P}3"
+else
+    die "Cannot determine current slot from ${CURRENT_ROOT}, expected ${P}2 or ${P}4."
+fi
+
+TARGET_SLOT="$([[ $CURRENT_SLOT == a ]] && echo b || echo a)"
+info "Current slot: $CURRENT_SLOT"
+info "Writing to slot: $TARGET_SLOT ($TARGET_ROOT)"
+
+# 3. Write rootfs and verity to the inactive slots
+info "Writing rootfs.erofs to $TARGET_ROOT..."
+dd if="$WORKDIR/rootfs.erofs" of="$TARGET_ROOT" bs=4M status=progress
+sync
+
+info "Writing rootfs.erofs.verity to $TARGET_VERITY..."
+dd if="$WORKDIR/rootfs.erofs.verity" of="$TARGET_VERITY" bs=4M status=progress
+sync
+
+# 4. Update bootloader and UKI in ESP
+info "Updating ESP..."
+mcopy -i "${P}1" "$WORKDIR/bootloader-signed.efi" ::/EFI/systemd/systemd-bootx64.efi
+mcopy -i "${P}1" "$WORKDIR/bootloader-signed.efi" ::/EFI/BOOT/BOOTX64.EFI
+mcopy -i "${P}1" "$WORKDIR/uki-${RELEASE_TAG}-signed.efi" ::/EFI/Linux/uki-${RELEASE_TAG}-signed.efi
+sync
+
+# 5. Reboot
+info "Update complete. Rebooting..."
+reboot
